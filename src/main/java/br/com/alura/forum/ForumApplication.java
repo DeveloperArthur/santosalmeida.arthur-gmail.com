67a875c6c1ecc17121305d4f489fa0c8de8c7c42
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2020-10-14 18:11:26 -0300

    Add files via upload

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,13 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+//Para utilizar a paginacao automatica é necessario adicionar esta anotação
+//@EnableSpringDataWebSupport
+@EnableCaching
 public class ForumApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,12 @@ import br.com.alura.forum.dominio.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -15,7 +21,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -27,18 +32,42 @@ public class TopicosController {
     private CursoRepository cursoRepository;
 
     @GetMapping
-    public List<TopicoDto> lista(String nomeCurso) {
+    @Cacheable(value = "listaDeTopicos")
+    public Page<TopicoDto> listaComPaginacaoNaMao(@RequestParam(required = false) String nomeCurso,
+                                 @RequestParam int pagina,
+                                 @RequestParam int quantidade,
+                                 @RequestParam String ordenacao) {
+        Pageable paginacao = PageRequest.of(pagina, quantidade, Sort.Direction.ASC, ordenacao);
+
         if (nomeCurso == null) {
-            List<Topico> topicos = topicoRepository.findAll();
+            Page<Topico> topicos = topicoRepository.findAll(paginacao);
             return TopicoDto.converter(topicos);
         } else {
-            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
             return TopicoDto.converter(topicos);
         }
     }
 
+//    @GetMapping
+//    public Page<TopicoDto> listaComPaginacaoAutomatica(@RequestParam(required = false) String nomeCurso,
+//                                                       @RequestParam Pageable paginacao) {
+//        //Para funcionar, é necessário ir na classe main e anotar com @EnableSpringDataWebSuppor
+//        //parametros: page, size, sort
+//        //é possível também adicionar uma @PageableDefault na frente do parmetro do controller
+//        //Exemplo: @PageableDefault(sort="id", direction=Direction.DES, page=0, size=10)
+//
+//        if (nomeCurso == null) {
+//            Page<Topico> topicos = topicoRepository.findAll(paginacao);
+//            return TopicoDto.converter(topicos);
+//        } else {
+//            Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
+//            return TopicoDto.converter(topicos);
+//        }
+//    }
+
     @PostMapping
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder builder) {
         Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);
@@ -58,6 +87,7 @@ public class TopicosController {
 
     @PutMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
         Optional<Topico> optional = topicoRepository.findById(id);
         if (optional.isPresent()) {
@@ -69,6 +99,7 @@ public class TopicosController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<?> remover(@PathVariable Long id) {
         Optional<Topico> optional = topicoRepository.findById(id);
         if (optional.isPresent()) {
